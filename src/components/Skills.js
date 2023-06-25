ad9050d97525fa526e8d9d56989873ab56f7fbc4
Yenthly-Devolder <yenthly.devolder@student.hogent.be>
2023-06-25 17:20:00 +0200

    updates

---
+++ src/components/Skills.js
@@ -0,0 +1,91 @@
+import Fade from "react-reveal/Fade";
+
+export default function () {
+  return (
+    <div className="skills-section">
+      <div className="skills-container">
+        <h2>SKILLS</h2>
+        <table>
+          <tr>
+            <Fade right>
+              <td >
+                <img src="./Javascript_Logo.png" alt="java-icon"></img>
+                <h3>Java</h3>
+              </td>
+            </Fade>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="ico"></img>
+                <h3>MySQL</h3>
+              </td>
+            </Fade>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="java-icon"></img>
+                <h3>JavaScript</h3>
+              </td>
+            </Fade>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="java-icon"></img>
+                <h3>JavaScript</h3>
+              </td>
+            </Fade>
+          </tr>
+          <tr>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="java-icon"></img>
+                <h3>Java</h3>
+              </td>
+            </Fade>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="ico"></img>
+                <h3>MySQL</h3>
+              </td>
+            </Fade>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="java-icon"></img>
+                <h3>JavaScript</h3>
+              </td>
+            </Fade>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="java-icon"></img>
+                <h3>JavaScript</h3>
+              </td>
+            </Fade>
+          </tr>
+          <tr>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="java-icon"></img>
+                <h3>Java</h3>
+              </td>
+            </Fade>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="ico"></img>
+                <h3>MySQL</h3>
+              </td>
+            </Fade>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="java-icon"></img>
+                <h3>JavaScript</h3>
+              </td>
+            </Fade>
+            <Fade right>
+              <td>
+                <img src="./Javascript_Logo.png" alt="java-icon"></img>
+                <h3>JavaScript</h3>
+              </td>
+            </Fade>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
